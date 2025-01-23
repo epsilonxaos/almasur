e7e08b9707cd4da0556ab0a5638ec1efeef1dac8
Daniel Amaya <daniel_amaya7398@hotmail.com>
2025-01-22 19:23:00 -0600

    fix(Etiqueta class => className):

--- resources/js/react/components/Loading.jsx
+++ resources/js/react/components/Loading.jsx
@@ -3,7 +3,7 @@ import "../../../css/loading.css";
 export default function Loading() {
     return (
         <div className="flex h-screen w-full items-center justify-center bg-white">
-            <div class="spinner"></div>
+            <div className="spinner"></div>
         </div>
     );
 }
