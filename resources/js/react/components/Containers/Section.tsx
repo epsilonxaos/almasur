90660b27a1e920d9ce9f76ed5ebac7ca681061dc
Daniel Amaya <daniel_amaya7398@hotmail.com>
2025-01-30 23:20:12 -0600

    chore(Otros): :zap: Se quitaron los bordes de apoyo

--- resources/js/react/components/Containers/Section.tsx
+++ resources/js/react/components/Containers/Section.tsx
@@ -11,10 +11,12 @@ const Section = (opt: TSection) => {
 	return (
 		// CONTENEDOR
 		<div
-			className={`${notFullWidth ? 'px-8' : ''} ${classContainer} mx-auto flex max-w-section flex-col items-center justify-center border-2 border-red-700 text-center`}>
+			// className={`${notFullWidth ? 'px-8' : ''} ${classContainer} mx-auto flex max-w-section flex-col items-center justify-center border-2 border-red-700 text-center`}>
+			className={`${notFullWidth ? 'px-8' : ''} ${classContainer} mx-auto flex max-w-section flex-col items-center justify-center  text-center`}>
 			{/* CONTENIDO */}
 			<div
-				className={`${className} text-main flex w-full max-w-section-content flex-col items-center justify-center border-2 border-blue-700 p-4 sm:p-8 ${className}`}>
+				// className={`${className} text-main flex w-full max-w-section-content flex-col items-center justify-center border-2 border-blue-700 p-4 sm:p-8 ${className}`}>
+				className={`${className} text-main flex w-full max-w-section-content flex-col items-center justify-center p-4 sm:p-8 ${className}`}>
 				{children}
 			</div>
 		</div>
