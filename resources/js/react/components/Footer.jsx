a87c18158fe513e44f7d797331a1527050359dd4
Daniel Amaya <daniel_amaya7398@hotmail.com>
2025-02-05 17:56:16 -0600

    style(Otros): :lipstick: Desfase de texto en footer, cuando pantalla pequeña

--- resources/js/react/components/Footer.jsx
+++ resources/js/react/components/Footer.jsx
@@ -1,12 +1,14 @@
+import { twMerge } from 'tailwind-merge'
+
 import { colorVariants } from '../utils/tailwindColors'
 import { TextSmallContent } from './Items/Text'
 
-const Footer = ({ color = 'pacifico' }) => {
+const Footer = ({ color = 'pacifico', className = '' }) => {
 	return (
-		<footer className={`${colorVariants[color].background} w-full px-4 py-5 text-main_b`}>
-			<ul className='font-geo-500 mx-auto flex w-full max-w-section-content flex-col items-center justify-between gap-2 md:flex-row'>
-				<TextSmallContent>TERMINOS Y CONDICIONES</TextSmallContent>
-				<TextSmallContent>
+		<footer className={twMerge(`${colorVariants[color].background} w-full px-4 py-5 text-main_b`, className)}>
+			<ul className='font-geo-500 mx-auto flex w-full max-w-section-content flex-col items-center justify-between gap-2 text-center md:flex-row'>
+				<TextSmallContent className='dev-cnt'>TERMINOS Y CONDICIONES</TextSmallContent>
+				<TextSmallContent className='dev-cnt'>
 					DISEÑO Y DESARROLLO POR{' '}
 					<a
 						href='https://www.madeby.partners/'
@@ -15,7 +17,7 @@ const Footer = ({ color = 'pacifico' }) => {
 						<span className='font-bold'>MadebyPartners®</span>
 					</a>
 				</TextSmallContent>
-				<TextSmallContent>POLÍTICAS DE PRIVACIDAD</TextSmallContent>
+				<TextSmallContent className='dev-cnt'>POLÍTICAS DE PRIVACIDAD</TextSmallContent>
 			</ul>
 		</footer>
 	)
